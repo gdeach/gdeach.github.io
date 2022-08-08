b99722140a0eaec440dde75d01efe5dede31c7e9
Gastón De Abreu <110435545+gdeach@users.noreply.github.com>
2022-08-08 03:00:51 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.onscroll = function(){
     var elemento1 = document.getElementById("icon__pag")
     var elemento2 = document.getElementById("icon__pag2")
 
-    elemento1.style.bottom = posicion * 0.2 + "px";
+    elemento1.style.bottom = posicion * 0.1 + "px";
     elemento2.style.top = posicion * 0.1 + "px";
 }
 document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
